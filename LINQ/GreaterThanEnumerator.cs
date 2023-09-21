bd4a987bb00ce81920bb944e079f0c76d193203b
Varlam Dadiani <varlam.dadiani@yahoo.com>
2023-09-22 02:24:54 +0400

    Add project files.

---
+++ LINQ/GreaterThanEnumerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+
+namespace LINQ
+{
+    public static class Test2
+    {
+        public static void Test()
+        {
+            var arr = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in new GreaterThanEnumerable(arr, 2))
+                Console.WriteLine(number);
+        }
+    }
+
+    public class GreaterThanEnumerable : IEnumerable<int>
+    {
+        private readonly IEnumerable<int> _enumerable;
+        private readonly int _number;
+
+        public GreaterThanEnumerable(IEnumerable<int> enumerable, int number)
+        {
+            _enumerable = enumerable;
+            _number = number;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new GreaterThanEnumerator(_enumerable, _number);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+    public class GreaterThanEnumerator : IEnumerator<int>
+    {
+        private readonly int _number;
+        private readonly IEnumerator<int> _enumerator;
+
+        public GreaterThanEnumerator(IEnumerable<int> enumerable, int number)
+        {
+            _number = number;
+            _enumerator = enumerable.GetEnumerator();
+        }
+
+        public int Current => _enumerator.Current;
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            while (_enumerator.MoveNext())
+            {
+                if (_enumerator.Current > _number)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            _enumerator.Reset();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ LINQ/VarlamEnumerable.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace LINQ
+{
+    public static class Test1
+    {
+        public static void Test()
+        {
+            var arr = new int[] { 1, 2, 3, 4 };
+
+            foreach (var number in new VarlamEnumerable<int>(arr))
+                Console.WriteLine(number);
+        }
+    }
+
+    public class VarlamEnumerable<T> : IEnumerable<T>
+    {
+        private readonly T[] _items;
+
+        public VarlamEnumerable(params T[] items)
+        {
+            _items = items;
+        }
+
+        public IEnumerator<T> GetEnumerator() => new VarlamEnumerator<T>(_items);
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class VarlamEnumerator<T> : IEnumerator<T>
+    {
+        private readonly T[] _array;
+        private int _position = -1;
+
+        public VarlamEnumerator(params T[] array)
+        {
+            _array = array;
+        }
+
+        public T Current => _array[_position];
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            return ++_position < _array.Length;
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+    }
+}
